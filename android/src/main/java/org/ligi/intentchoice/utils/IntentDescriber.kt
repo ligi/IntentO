59358137ce5461bd85154d2c94197f874628c182
ligi <ligi@ligi.de>
2016-07-31 17:07:12 +0200

    Remove guava / convert more code to kotlin

---
+++ android/src/main/java/org/ligi/intentchoice/utils/IntentDescriber.kt
@@ -0,0 +1,41 @@
+package org.ligi.intentchoice.utils
+
+import android.content.Intent
+
+class IntentDescriber(private val intent: Intent) {
+
+    val userFacingCategoriesString: String
+        get() {
+            val categories = intent.categories.joinToString(separator = " ")
+            return categories.replace("android\\.intent\\.category\\.".toRegex(), "")
+        }
+
+    val claspedCategoriesString: String
+        get() {
+            if (intent.categories == null) {
+                return ""
+            }
+
+            if (intent.categories.size > 1) {
+                return "($userFacingCategoriesString)"
+            } else {
+                return userFacingCategoriesString
+            }
+        }
+
+    val userFacingActionString: String
+        get() = intent.action.replace("android.intent.action.", "")
+
+    val userFacingIntentDescription: String
+        get() {
+            val res = StringBuilder(userFacingActionString)
+            if (intent.categories != null) {
+                res.append("@" + claspedCategoriesString)
+            }
+            if (intent.data != null) {
+                res.append(intent.data)
+            }
+            return res.toString()
+        }
+
+}
