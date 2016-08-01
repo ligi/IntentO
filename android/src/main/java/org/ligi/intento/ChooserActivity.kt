88306947bbbb27a50dbe9ac23d3c8755fd072d0b
ligi <ligi@ligi.de>
2016-08-01 12:36:32 +0200

    Initial Info-Activity, Changing Icon for notification

--- android/src/main/java/org/ligi/intento/ChooserActivity.kt
+++ android/src/main/java/org/ligi/intento/ChooserActivity.kt
@@ -12,6 +12,7 @@ import android.content.pm.ResolveInfo
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
@@ -22,20 +23,23 @@ import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
+
 import org.ligi.axt.AXT
 import org.ligi.intento.utils.IntentDescriber
-import java.util.ArrayList
 
 class ChooserActivity : AppCompatActivity() {
 
     val actionTextView by lazy { findViewById(R.id.action) as TextView }
     val dataTextView by lazy { findViewById(R.id.data) as TextView }
+    val extrasValTextView by lazy { findViewById(R.id.extras_val) as TextView }
     val dataValTextView by lazy { findViewById(R.id.data_val) as TextView }
     val catTextView by lazy { findViewById(R.id.categories) as TextView }
     val catValTextView by lazy { findViewById(R.id.categories_val) as TextView }
     val alwaysCheckBox by lazy { findViewById(R.id.always_checkbox) as CheckBox }
     val addConditionButton by lazy { findViewById(R.id.add_condition) as Button }
     val intentList by lazy { findViewById(R.id.intentList) as RecyclerView }
+
+    val NOTIFICATION_ID = 1;
 
     private var intentDescriber: IntentDescriber? = null
 
@@ -49,18 +53,15 @@ class ChooserActivity : AppCompatActivity() {
 
         intent.categories?.forEach { targetIntent.addCategory(it) }
 
+        targetIntent.putExtras(intent)
+
+        targetIntent.type = intent.type
+
         targetIntent.data = intent.data
 
-        val resolveInfoList = pm.queryIntentActivities(targetIntent, PackageManager.MATCH_DEFAULT_ONLY)
+        val resolveInfoList = pm.queryIntentActivities(targetIntent, PackageManager.MATCH_ALL)
 
-        val filteredResolveInfoList = ArrayList<ResolveInfo>()
-
-        for (resolveInfo in resolveInfoList) {
-            // exclude our stuff
-            if (resolveInfo.activityInfo.packageName != packageName) {
-                filteredResolveInfoList.add(resolveInfo)
-            }
-        }
+        val filteredResolveInfoList = resolveInfoList.filter { !it.activityInfo.packageName.equals(packageName) }
 
         // if we have only one result left we can safely choose it
         if (filteredResolveInfoList.size == 1) {
@@ -68,13 +69,15 @@ class ChooserActivity : AppCompatActivity() {
             return
         }
 
-        setUpUI(filteredResolveInfoList)
+        setUpUI(filteredResolveInfoList, targetIntent)
+
+        supportActionBar?.subtitle = intentDescriber!!.userFacingIntentDescription
     }
 
-    private fun setUpUI(filteredResolveInfoList: List<ResolveInfo>) {
+    private fun setUpUI(filteredResolveInfoList: List<ResolveInfo>, intent: Intent) {
         setContentView(R.layout.activity_chooser)
 
-        showIntentDetails()
+        showIntentDetails(intent)
 
         setupIntentList(filteredResolveInfoList)
 
@@ -108,7 +111,9 @@ class ChooserActivity : AppCompatActivity() {
 
         val notification = notification(bitmap, contentIntent, label)
 
-        notificationManager.notify(1, notification)
+        notificationManager.cancel(NOTIFICATION_ID) // we need to cancel the old notification as we cannot update the notification with dynamic icons
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
 
         val component = ComponentName(resolveInfo.activityInfo.packageName, resolveInfo.activityInfo.name)
         Log.i("Intentify", " start Intent " + resolveInfo.activityInfo.packageName + " " + resolveInfo.activityInfo.name)
@@ -119,7 +124,7 @@ class ChooserActivity : AppCompatActivity() {
 
     @TargetApi(23)
     private fun notification(bitmap: Bitmap?, contentIntent: PendingIntent?, label: CharSequence?): Notification? {
-        val notification = if (BuildConfig.VERSION_CODE < 23) {
+        val notification = if (Build.VERSION.SDK_INT < 23) {
             NotificationCompat.Builder(this)
                     .setLargeIcon(bitmap)
                     .setSmallIcon(R.drawable.ic_launcher)
@@ -143,10 +148,10 @@ class ChooserActivity : AppCompatActivity() {
     }
 
 
-    private fun showIntentDetails() {
+    private fun showIntentDetails(intent: Intent) {
         if (intent.action != null) {
             // remove redundant information
-            actionTextView.text = intentDescriber!!.userFacingActionString
+            actionTextView.text = intentDescriber!!.userFacingActionString + " " + intent.type
         } else {
             actionTextView.visibility = View.GONE
         }
@@ -156,6 +161,13 @@ class ChooserActivity : AppCompatActivity() {
         } else {
             dataTextView.visibility = View.GONE
             dataValTextView.visibility = View.GONE
+        }
+
+        if (intent.extras != null) {
+            var foo = ""
+            intent.extras.keySet().forEach { foo += it + " " + intent.extras.get(it) + "\n" }
+
+            extrasValTextView.text = foo
         }
 
         if (intent.categories != null) {
---
+++ android/src/main/java/org/ligi/intento/IntroActivity.kt
@@ -0,0 +1,14 @@
+package org.ligi.intento
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_info)
+    }
+
+}
